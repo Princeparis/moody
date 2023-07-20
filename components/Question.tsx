a569821c6f44d3b8e1b369bc39a310d63e039056
Yusuff Ridwan <wemzain@gmail.com>
2023-07-20 17:00:47 +0100

    added ai question feature

---
+++ components/Question.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { askQuestion } from '@/utils/api'
+import { useState } from 'react'
+
+const Question = () => {
+  const [value, setValue] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [response, setResponse] = useState('')
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setLoading(true)
+    e.preventDefault()
+    const answer = await askQuestion(value)
+    setResponse(answer)
+    setValue('')
+    setLoading(false)
+  }
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex justify-between items-center pr-1 pl-2 py-1 bg-white rounded-full w-[430px]"
+      >
+        <input
+          disabled={loading}
+          value={value}
+          onChange={handleChange}
+          type="text"
+          placeholder="Ask any question"
+          className="px-4 py-3 text-lg w-full outline-none"
+        />
+        <button
+          disabled={loading}
+          type="submit"
+          className="px-6 py-3 bg-purple-600 text-lg font-bold rounded-full text-white"
+        >
+          Ask
+        </button>
+      </form>
+      {loading && <div>...loading</div>}
+      {response && <div className="w-[560px]">{response}</div>}
+    </div>
+  )
+}
+
+export default Question
--- utils/ai.ts
+++ utils/ai.ts
@@ -1,51 +1,76 @@
-import { get } from "http";
-import { OpenAI } from "langchain/llms/openai";
-import { StructuredOutputParser } from "langchain/output_parsers";
-import { PromptTemplate } from "langchain/prompts";
-import z from "zod";
-
+import { get } from 'http'
+import { OpenAI } from 'langchain/llms/openai'
+import { StructuredOutputParser } from 'langchain/output_parsers'
+import { PromptTemplate } from 'langchain/prompts'
+import z from 'zod'
+import { Document } from 'langchain/document'
+import { loadQARefineChain } from 'langchain/chains'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
     mood: z
       .string()
-      .describe("the mood of the person who wrote the journal entry."),
-    summary: z.string().describe("quick summary of the entire entry."),
+      .describe('the mood of the person who wrote the journal entry.'),
+    summary: z.string().describe('quick summary of the entire entry.'),
     negative: z
       .boolean()
       .describe(
-        "is the journal entry negative? (i.e. does it contain negative emotions?)."
+        'is the journal entry negative? (i.e. does it contain negative emotions?).'
       ),
-    subject: z.string().describe("the subject of the journal entry."),
+    subject: z.string().describe('the subject of the journal entry.'),
     color: z
       .string()
       .describe(
-        "a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness."
+        'a hexidecimal color code that represents the mood of the entry. Example #0101fe for blue representing happiness.'
       ),
   })
-);
+)
 
 const getPrompt = async (content: string) => {
-  const formattedInstruction = parser.getFormatInstructions();
+  const formattedInstruction = parser.getFormatInstructions()
   const prompt = new PromptTemplate({
     template:
-      "Analyze the following journal entry. Follow the intructions and format your response to match the format instructions, no matter what! \n{formattedInstruction}\n{entry}",
-    inputVariables: ["entry"],
+      'Analyze the following journal entry. Follow the intructions and format your response to match the format instructions, no matter what! \n{formattedInstruction}\n{entry}',
+    inputVariables: ['entry'],
     partialVariables: { formattedInstruction },
-  });
+  })
 
   const input = await prompt.format({
     entry: content,
-  });
-  return input;
-};
+  })
+  return input
+}
 export const analyze = async (content: string) => {
-  const input = await getPrompt(content);
-  const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
-  const result = await model.call(input);
+  const input = await getPrompt(content)
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const result = await model.call(input)
   try {
-    return parser.parse(result);
+    return parser.parse(result)
   } catch (error: Error | any) {
-    console.log(error.messaage);
-    return null;
+    console.log(error.messaage)
+    return null
   }
-};
+}
+
+export const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: {
+        id: entry.id,
+        createdAt: entry.createdAt,
+      },
+    })
+  })
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+  return res.output_text
+}
